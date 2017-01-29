57f2ba843c292ad1bd2b88eea20348a200b9b883
Jonathan Lévy <jonathan.levy@ens-rennes.fr>
2017-01-29 22:34:29 +0100

    Stage 1 : locate file, open file, close file. No functions or objects created for the moment...

--- testsuite/main.cpp
+++ testsuite/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
-using namespace std;
-
-int main()
+int main(int argc, char **argv)
 {
-    cout << "Hello world!" << endl;
+    std::cout << "Hello world!" << std::endl;
+    std::string file = "";
+    if (argc == 2)
+    {
+        file = argv[1];
+    }
+    else
+    {
+        file = "test01";
+    }
+    std::ifstream testfile;
+    testfile.open(file.c_str());
+    // équivalent à std::ifstream testfile(string);
+
+    std::string line;
+    std::getline(testfile, line);
+    std::cout << line << std::endl;
+
+    testfile.close();
+
+
     return 0;
 }
